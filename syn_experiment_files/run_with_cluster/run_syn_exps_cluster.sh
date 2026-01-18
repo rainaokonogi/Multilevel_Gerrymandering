6162d35bc6d58055ef247983e53f5044c5d8fe3b
rainaokonogi <reo39@cornell.edu>
2026-01-18 00:44:57 -0600

    Fixed typo.

--- syn_experiment_files/run_with_cluster/run_syn_exps_cluster.sh
+++ syn_experiment_files/run_with_cluster/run_syn_exps_cluster.sh
@@ -20,7 +20,7 @@ for random_seed in {1..5}; do
                             --time=4-00:00:00 \
                             --error="syn_error_files/syn_exps_r_${num_r_units}_map_${map_number}_block_size_${block_size}_${experiment_type}_init_part_${init_part}_seed_${random_seed}.log" \
                             --output="syn_output_files/syn_output_r_${num_r_units}_map_${map_number}_block_size_${block_size}_${experiment_type}_init_part_${init_part}_seed_${random_seed}.out" \
-                            --wrap="PATHONHASHSEED=0 uv run ${TOP_DIR}/syn_exps_cli.py --num-r-units $num_r_units --map-number $map_number --block-size $block_size --experiment-type $experiment_type --init-part $init_part --random-seed $random_seed --total-steps 20000"
+                            --wrap="PYTHONHASHSEED=0 uv run ${TOP_DIR}/syn_exps_cli.py --num-r-units $num_r_units --map-number $map_number --block-size $block_size --experiment-type $experiment_type --init-part $init_part --random-seed $random_seed --total-steps 20000"
                     done
                 done
             done
